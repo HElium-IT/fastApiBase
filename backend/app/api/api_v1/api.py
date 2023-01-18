bbca94f7d618fd5a71bb90d27f9e7417957aeefe
HeliumP <89072665+HeliumP@users.noreply.github.com>
2023-01-18 16:14:04 +0100

    typo-fix

--- backend/app/api/api_v1/api.py
+++ backend/app/api/api_v1/api.py
@@ -3,8 +3,8 @@ from fastapi import APIRouter
 
 from app.api.api_v1.endpoints import items, login, users, utils
 
-api_v1 = APIRouter(prefix=settings.API_V1_STR)
-api_v1.include_router(login.router, tags=["login"])
-api_v1.include_router(users.router, prefix="/users", tags=["users"])
-api_v1.include_router(utils.router, prefix="/utils", tags=["utils"])
-api_v1.include_router(items.router, prefix="/items", tags=["items"])
+api_router = APIRouter(prefix=settings.API_V1_STR)
+api_router.include_router(login.router, tags=["login"])
+api_router.include_router(users.router, prefix="/users", tags=["users"])
+api_router.include_router(utils.router, prefix="/utils", tags=["utils"])
+api_router.include_router(items.router, prefix="/items", tags=["items"])
